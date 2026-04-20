4dc094958ea885d12b47e49ed03586eb81a642c1
jerinjackj17 <jjerinjack@gmail.com>
2026-04-20 02:21:20 -0500

    docker compose fix

--- core-banking/src/main/java/com/bankofgeorgia/core_banking/CoreBankingApplication.java
+++ core-banking/src/main/java/com/bankofgeorgia/core_banking/CoreBankingApplication.java
@@ -2,6 +2,9 @@ package com.bankofgeorgia.core_banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoreBankingApplication {
@@ -9,5 +12,20 @@ public class CoreBankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreBankingApplication.class, args);
 	}
+	
+	
 
+@Bean
+CommandLineRunner printMongoConfig(
+        @Value("${spring.data.mongodb.host:missing}") String host,
+        @Value("${spring.data.mongodb.port:missing}") String port,
+        @Value("${spring.data.mongodb.database:missing}") String database,
+        @Value("${spring.data.mongodb.uri:missing}") String uri) {
+    return args -> {
+        System.out.println("MONGO_HOST=" + host);
+        System.out.println("MONGO_PORT=" + port);
+        System.out.println("MONGO_DB=" + database);
+        System.out.println("MONGO_URI=" + uri);
+    };
+}
 }
